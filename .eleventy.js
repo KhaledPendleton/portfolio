cc7510bad0db5bcfd7053babaca79946678baf74
Khaled Pendleton <hi@khaledpendleton.com>
2022-03-10 22:00:17 -0500

    FEAT (Data): Published 11ty collection of work data

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order');
+
 module.exports = (config) => {
     config.addPassthroughCopy('./src/images/');
-    
+
+    // Returns work items, sorted by display order
+    config.addCollection('work', (collection) => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
